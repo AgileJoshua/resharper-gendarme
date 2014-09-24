7e8afa19f3a1a46f12d73c8be744a63c9a2cc75d
Anton Zhidkov <anton.v.zhidkov@gmail.com>
2014-09-24 19:44:30 +0400

    Added quick fix and stubs for bulb items.

---
+++ src/RGendarme/RGendarme/RGendarme/Rules/Design/AvoidVisibleFields/AvoidVisibleFieldsCreatePropertyBulbItem.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Intentions.Extensibility;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.TextControl;
+
+namespace RGendarme.Rules.Design.AvoidVisibleFields
+{
+    public class AvoidVisibleFieldsCreatePropertyBulbItem : IBulbAction
+    {
+        private readonly IMultipleFieldDeclaration _declaration;
+        public AvoidVisibleFieldsCreatePropertyBulbItem(IMultipleFieldDeclaration declaration)
+        {
+            _declaration = declaration;
+        }
+
+        public void Execute(ISolution solution, ITextControl textControl)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Text { get { return "Convert to property"; } }
+    }
+}

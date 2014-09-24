ec016e480230d91d2f6d77502e2be879d8834c86
Anton Zhidkov <anton.v.zhidkov@gmail.com>
2014-09-24 17:55:51 +0400

    Added availability tests for StringIsNullOrEmptyWithFalse rule.

---
+++ src/RGendarme/RGendarme/RGendarme.Tests/Rules/Custom/StringIsNullOrEmptyWithFalse/StringIsNullOrEmptyWithFalseAvailabilityTests.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.ReSharper.Daemon;
+using JetBrains.ReSharper.Intentions.Test;
+using JetBrains.ReSharper.Psi;
+using NUnit.Framework;
+using RGendarme.Rules.Custom.StringIsNullOrEmptyWithFalse;
+
+namespace RGendarme.Tests.Rules.Custom.StringIsNullOrEmptyWithFalse
+{
+    [TestFixture]
+    public class StringIsNullOrEmptyWithFalseAvailabilityTests : QuickFixAvailabilityTestBase
+    {
+        protected override string RelativeTestDataPath
+        {
+            get { return @"Rules\Custom\StringIsNullOrEmptyWithFalse"; }
+        }
+
+        protected override bool HighlightingPredicate(IHighlighting highlighting, IPsiSourceFile psiSourceFile)
+        {
+            return highlighting is StringIsNullOrEmptyWithFalseHighlight;
+        }
+
+        [Test]
+        public void Availability01()
+        {
+            DoTestFiles("availability01.cs");
+        }
+    }
+}
---
+++ src/RGendarme/RGendarme/RGendarme.Tests/test/data/Rules/Custom/StringIsNullOrEmptyWithFalse/availability01.cs
@@ -0,0 +1,17 @@
+﻿class C1
+{
+    static void Main(string[] args)
+    {
+        string s = "abc";
+
+        double x = 0.0;
+        double y = System.Math.Pow(x, 2.0);
+        double z = System.Math.Pow(x, 3.0);
+        double n = System.Math.Pow(x, 4.0);
+
+        if (string.IsNullOrEmpty("hello world") == false)
+        {
+
+        }
+    }
+}

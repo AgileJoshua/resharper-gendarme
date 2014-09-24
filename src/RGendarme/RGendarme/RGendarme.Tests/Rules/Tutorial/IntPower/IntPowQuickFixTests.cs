79b90d8d5c43e0747b73c116b17eba3c2834e714
Anton Zhidkov <anton.v.zhidkov@gmail.com>
2014-09-24 10:34:51 +0400

    Working availability tests.

--- src/RGendarme/RGendarme/RGendarme.Tests/Rules/Tutorial/IntPower/IntPowQuickFixTests.cs
+++ src/RGendarme/RGendarme/RGendarme.Tests/Rules/Tutorial/IntPower/IntPowQuickFixTests.cs
@@ -1,5 +1,4 @@
-﻿using JetBrains.ReSharper.Intentions.Test;
-using JetBrains.ReSharper.IntentionsTests;
+﻿using JetBrains.ReSharper.IntentionsTests;
 using NUnit.Framework;
 using RGendarme.Rules.Tutorial.IntPower;
 
@@ -14,35 +13,10 @@ namespace RGendarme.Tests.Rules.Tutorial.IntPower
             get { return @"Rules\Tutorial\IntPower"; }
         }
 
-//        protected override bool HighlightingPredicate(JetBrains.ReSharper.Daemon.IHighlighting highlighting, JetBrains.ReSharper.Psi.IPsiSourceFile psiSourceFile)
-//        {
-//            return highlighting is IntPowerHighlighting;
-//        }
-
         [Test]
         public void QuickFix01()
         {
             DoTestFiles("quickfix01.cs.txt");
-        }
-    }
-
-    [TestFixture]
-    public class IntPowQuickFixAvailabilityTests : QuickFixAvailabilityTestBase
-    {
-        protected override string RelativeTestDataPath
-        {
-            get { return @"Rules\Tutorial\IntPower"; }
-        }
-
-        protected override bool HighlightingPredicate(JetBrains.ReSharper.Daemon.IHighlighting highlighting, JetBrains.ReSharper.Psi.IPsiSourceFile psiSourceFile)
-        {
-            return highlighting is IntPowerHighlighting;
-        }
-
-        [Test]
-        public void Availability01()
-        {
-            DoTestFiles("availability01.cs");
         }
     }
 }

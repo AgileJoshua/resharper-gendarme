d13cf235b324bf3b906e4f4146783ad1e62fe0ae
Anton Zhidkov <anton.v.zhidkov@gmail.com>
2014-09-29 12:42:56 +0400

    Added analyzer and highlight stub UseCorrectSuffix/Attribute

---
+++ src/RGendarme/RGendarme/RGendarme/Rules/Naming/UseCorrectSuffix/Attribute/NotImplementType/UseCorrectSuffixAttributeNotImplementAttributeAnalyzer.cs
@@ -0,0 +1,40 @@
+﻿using JetBrains.ReSharper.Daemon;
+using JetBrains.ReSharper.Daemon.Stages;
+using JetBrains.ReSharper.Daemon.Stages.Dispatcher;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace RGendarme.Rules.Naming.UseCorrectSuffix.Attribute.NotImplementType
+{
+    [ElementProblemAnalyzer(new[] { typeof(IClassDeclaration) }, HighlightingTypes = new[] { typeof(UseCorrectSuffixAttributeNotImplementAttributeHighlighting) })]
+    public class UseCorrectSuffixAttributeNotImplementAttributeAnalyzer : ElementProblemAnalyzer<IClassDeclaration>
+    {
+        protected override void Run(IClassDeclaration element, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
+        {
+            // 1. get class name
+            // 2. get exntend list
+            // 3. check if class has Attribute suffix but doesn't extend Attribute class
+
+            throw new System.NotImplementedException();
+        }
+    }
+
+    [StaticSeverityHighlighting(Severity.WARNING, CSharpLanguage.Name)]
+    public class UseCorrectSuffixAttributeNotImplementAttributeHighlighting : IHighlighting
+    {
+        public IClassDeclaration ClassDeclaration { get; private set; }
+        public UseCorrectSuffixAttributeNotImplementAttributeHighlighting(IClassDeclaration classDeclaration)
+        {
+            ClassDeclaration = classDeclaration;
+        }
+
+        public bool IsValid()
+        {
+            return ClassDeclaration != null && ClassDeclaration.IsValid();
+        }
+
+        public string ToolTip { get { return "Has Attribute suffix but doesn't extend Attribute class"; } }
+        public string ErrorStripeToolTip { get { return ToolTip; } }
+        public int NavigationOffsetPatch { get { return 0; }}
+    }
+}
---
+++ src/RGendarme/RGendarme/RGendarme/Rules/Naming/UseCorrectSuffix/Attribute/WrongSuffix/UseCorrectSuffixAttributeWrongClassNameAnalyzer.cs
@@ -0,0 +1,41 @@
+﻿using JetBrains.ReSharper.Daemon;
+using JetBrains.ReSharper.Daemon.Stages;
+using JetBrains.ReSharper.Daemon.Stages.Dispatcher;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+
+namespace RGendarme.Rules.Naming.UseCorrectSuffix.Attribute.WrongSuffix
+{
+    [ElementProblemAnalyzer(new[] { typeof(IClassDeclaration) }, HighlightingTypes = new[] { typeof(UseCorrectSuffixAttributeWrongClassNameHighlighting) })]
+    public class UseCorrectSuffixAttributeWrongClassNameAnalyzer : ElementProblemAnalyzer<IClassDeclaration>
+    {
+        protected override void Run(IClassDeclaration element, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
+        {
+            // 1. get class name
+            // 2. get exntend list
+            // 3. check if class extends Attribute class, it has to have Attbite suffix
+
+            throw new System.NotImplementedException();
+        }
+    }
+
+    // todo also add use case when class has Attribute suffix but doesn't extend Attribute class
+    [StaticSeverityHighlighting(Severity.WARNING, CSharpLanguage.Name)]
+    public class UseCorrectSuffixAttributeWrongClassNameHighlighting : IHighlighting
+    {
+        public IClassDeclaration ClassDeclaration { get; private set; }
+        public UseCorrectSuffixAttributeWrongClassNameHighlighting(IClassDeclaration classDeclaration)
+        {
+            ClassDeclaration = classDeclaration;
+        }
+
+        public bool IsValid()
+        {
+            return ClassDeclaration != null && ClassDeclaration.IsValid();
+        }
+
+        public string ToolTip { get { return "Class doesn't have Attbite suffix"; } }
+        public string ErrorStripeToolTip { get { return ToolTip; } }
+        public int NavigationOffsetPatch { get { return 0; } }
+    }
+}

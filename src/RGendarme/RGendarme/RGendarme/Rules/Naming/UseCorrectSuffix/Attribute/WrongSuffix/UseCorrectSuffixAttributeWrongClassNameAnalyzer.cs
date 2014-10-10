9ac87419d3fbb8643b64248e655212c4c0714b60
Anton Zhidkov <anton.v.zhidkov@gmail.com>
2014-10-10 13:03:02 +0400

    Added analyzer for Rules.Naming.UseCorrectSuffix.Attribute.WrongSuffix

--- src/RGendarme/RGendarme/RGendarme/Rules/Naming/UseCorrectSuffix/Attribute/WrongSuffix/UseCorrectSuffixAttributeWrongClassNameAnalyzer.cs
+++ src/RGendarme/RGendarme/RGendarme/Rules/Naming/UseCorrectSuffix/Attribute/WrongSuffix/UseCorrectSuffixAttributeWrongClassNameAnalyzer.cs
@@ -3,6 +3,7 @@ using JetBrains.ReSharper.Daemon.Stages;
 using JetBrains.ReSharper.Daemon.Stages.Dispatcher;
 using JetBrains.ReSharper.Psi.CSharp;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Tree;
 
 namespace RGendarme.Rules.Naming.UseCorrectSuffix.Attribute.WrongSuffix
 {
@@ -12,11 +13,37 @@ namespace RGendarme.Rules.Naming.UseCorrectSuffix.Attribute.WrongSuffix
         protected override void Run(IClassDeclaration element, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
         {
             // TODO: NotImplemented
-            // 1. get class name
-            // 2. get exntend list
-            // 3. check if class extends Attribute class, it has to have Attbite suffix
+            // 1. get exntend list
+            IExtendsList extends = element.ExtendsList;
+            if (extends == null) 
+                return;
 
-            //throw new System.NotImplementedException();
+            // 2. check if class extends Attribute class, it has to have Attbite suffix
+            bool isExtendAttribute = false;
+            foreach (IDeclaredTypeUsage type in extends.ExtendedInterfaces)
+            {
+                var declaredType = type as IUserDeclaredTypeUsage;
+                if (declaredType != null)
+                {
+                    if (declaredType.TypeName != null)
+                    {
+                        string extendTypeName = declaredType.TypeName.ShortName;
+                        if (extendTypeName.Equals("Attribute"))
+                            isExtendAttribute = true;
+                    }
+                }
+            }
+
+            if (!isExtendAttribute) 
+                return;
+
+            // 3. get class name
+            string name = element.NameIdentifier.Name;
+            if (!string.IsNullOrEmpty(name) && !name.EndsWith("Attribute"))
+            {
+                ICSharpIdentifier identifier = element.NameIdentifier;
+                consumer.AddHighlighting(new UseCorrectSuffixAttributeWrongClassNameHighlighting(identifier), identifier.GetDocumentRange(), identifier.GetContainingFile());
+            }
         }
     }
 
@@ -24,8 +51,8 @@ namespace RGendarme.Rules.Naming.UseCorrectSuffix.Attribute.WrongSuffix
     [StaticSeverityHighlighting(Severity.WARNING, CSharpLanguage.Name)]
     public class UseCorrectSuffixAttributeWrongClassNameHighlighting : IHighlighting
     {
-        public IClassDeclaration ClassDeclaration { get; private set; }
-        public UseCorrectSuffixAttributeWrongClassNameHighlighting(IClassDeclaration classDeclaration)
+        public ICSharpIdentifier ClassDeclaration { get; private set; }
+        public UseCorrectSuffixAttributeWrongClassNameHighlighting(ICSharpIdentifier classDeclaration)
         {
             ClassDeclaration = classDeclaration;
         }

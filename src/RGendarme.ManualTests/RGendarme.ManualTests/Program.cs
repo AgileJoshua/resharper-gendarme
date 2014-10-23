da5ba63d175408f588f9e7975f13589ed5e38e1f
Anton Zhidkov <anton.v.zhidkov@gmail.com>
2014-10-23 12:40:50 +0400

    TypesWithNativeFieldsShouldBeDisposable v 0.5

--- src/RGendarme.ManualTests/RGendarme.ManualTests/Program.cs
+++ src/RGendarme.ManualTests/RGendarme.ManualTests/Program.cs
@@ -6,11 +6,11 @@ using System.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Security.Policy;
 
-namespace One.Two.Three.Four.Five
-{
-    internal class Helper
-    {
-    }
+//namespace One.Two.Three.Four.Five
+//{
+//    internal class Helper
+//    {
+//    }
 
 //    public sealed class MyClass
 //    {
@@ -21,213 +21,224 @@ namespace One.Two.Three.Four.Five
 //        }
 //    }
 
-}
+//}
 
 namespace RGendarme.ManualTests
 {
-    public sealed class MyClass1
+//    public sealed class MyClass1
+//    {
+//        private int someValue;
+//    }
+//
+//    // =======================
+//    public sealed class MyClass2
+//    {
+//        private int GetAnswer()
+//        {
+//            return 42;
+//        }
+//    }
+
+    abstract public class AbstractDispose // : IDisposable
     {
-        private int someValue;
+        IntPtr field;
+
+        // the field should be disposed in the type that declares it
+//        public void Dispose()
+//        {
+//            
+//        }
     }
 
-    // =======================
-    public sealed class MyClass2
-    {
-        private int GetAnswer()
-        {
-            return 42;
-        }
-    }
 
-
-    class DoesNotOverloadAdd
-    {
-        public void Foo()
-        {
-            var rules = new Dictionary<string, string>
-	        {
-    	        {"-", "+"},
-    	        {"+", "-"},
-    	        {"+", "/"} // <-- error in runtime!
-	        };
-
-            var rule1 = new Dictionary<int, string>
-	        {
-    	        {1, "+"},
-    	        {2, "-"},
-    	        {1, "/"} // <-- error in runtime!
-	        };
-
-           
-        }
-
-        public bool NextJob (ref int id, string display)
-        {
-            display = String.Format("Job #{0}", id++);
-
-            if (id < 0)
-                return false;
-            
-            return true;
-        }
-
-        public static int operator - (DoesNotOverloadAdd left, DoesNotOverloadAdd right)
-        {
-            return 0;
-        }
-
-        public static int operator + (DoesNotOverloadAdd left, DoesNotOverloadAdd right)
-        {
-            return 0;
-        }
-
-//        public static int operator *(DoesNotOverloadAdd left, DoesNotOverloadAdd right)
+//    class DoesNotOverloadAdd
+//    {
+//        public void Foo()
 //        {
-//            return 0;
-//        }
-
-        public static int operator /(DoesNotOverloadAdd left, DoesNotOverloadAdd right)
-        {
-            return 0;
-        }
-
-//        public static int operator >(DoesNotOverloadAdd left, DoesNotOverloadAdd right)
-//        {
-//            return 0;
+//            var rules = new Dictionary<string, string>
+//	        {
+//    	        {"-", "+"},
+//    	        {"+", "-"},
+//    	        {"+", "/"} // <-- error in runtime!
+//	        };
+//
+//            var rule1 = new Dictionary<int, string>
+//	        {
+//    	        {1, "+"},
+//    	        {2, "-"},
+//    	        {1, "/"} // <-- error in runtime!
+//	        };
+//
+//           
 //        }
 //
-//        public static int operator <(DoesNotOverloadAdd left, DoesNotOverloadAdd right)
+//        public bool NextJob (ref int id, string display)
 //        {
-//            return 0;
-//        }
+//            display = String.Format("Job #{0}", id++);
 //
-//        public static int operator >=(DoesNotOverloadAdd left, DoesNotOverloadAdd right)
-//        {
-//            return 0;
-//        }
-
-//        public static int operator <=(DoesNotOverloadAdd left, DoesNotOverloadAdd right)
-//        {
-//            return 0;
-//        }
-
-//        public static bool operator true(DoesNotOverloadAdd ope)
-//        {
+//            if (id < 0)
+//                return false;
+//            
 //            return true;
 //        }
-
-//        public static bool operator false(DoesNotOverloadAdd ope)
+//
+//        public static int operator - (DoesNotOverloadAdd left, DoesNotOverloadAdd right)
 //        {
-//            return false;
+//            return 0;
 //        }
+//
+//        public static int operator + (DoesNotOverloadAdd left, DoesNotOverloadAdd right)
+//        {
+//            return 0;
+//        }
+//
+////        public static int operator *(DoesNotOverloadAdd left, DoesNotOverloadAdd right)
+////        {
+////            return 0;
+////        }
+//
+//        public static int operator /(DoesNotOverloadAdd left, DoesNotOverloadAdd right)
+//        {
+//            return 0;
+//        }
+//
+////        public static int operator >(DoesNotOverloadAdd left, DoesNotOverloadAdd right)
+////        {
+////            return 0;
+////        }
+////
+////        public static int operator <(DoesNotOverloadAdd left, DoesNotOverloadAdd right)
+////        {
+////            return 0;
+////        }
+////
+////        public static int operator >=(DoesNotOverloadAdd left, DoesNotOverloadAdd right)
+////        {
+////            return 0;
+////        }
+//
+////        public static int operator <=(DoesNotOverloadAdd left, DoesNotOverloadAdd right)
+////        {
+////            return 0;
+////        }
+//
+////        public static bool operator true(DoesNotOverloadAdd ope)
+////        {
+////            return true;
+////        }
+//
+////        public static bool operator false(DoesNotOverloadAdd ope)
+////        {
+////            return false;
+////        }
+//
+//    }
+//
+//    public class CPhone
+//    {
+//        public string IsConnectionString()
+//        {
+//            return "hello world";
+//        }
+//    }
+//
+//    [AttributeUsage(AttributeTargets.All)]
+//    public class MyAttribute : Attribute
+//    {
+//        
+//    }
+//
+//    public class Call<Mechanism>
+//    {
+//        // ...
+//    }
 
-    }
-
-    public class CPhone
-    {
-        public string IsConnectionString()
-        {
-            return "hello world";
-        }
-    }
-
-    [AttributeUsage(AttributeTargets.All)]
-    public class MyAttribute : Attribute
-    {
-        
-    }
-
-    public class Call<Mechanism>
-    {
-        // ...
-    }
-
-    [Serializable]
-//    [Flags]
-    enum Options
-    {
-        First = 1,
-        Second = 2,
-        Third = 4,
-        All = First | Second | Third,
-    }
+//    [Serializable]
+////    [Flags]
+//    enum Options
+//    {
+//        First = 1,
+//        Second = 2,
+//        Third = 4,
+//        All = First | Second | Third,
+//    }
     
 
-    public delegate void StringListEvent(IStringList sender);
-    public interface IStringList
-    {
-        // void Add(string s);
-        //       int Count { get; }
-        //        event StringListEvent Changed;
-        //         string this[int index] { get; set; }
-    }
+//    public delegate void StringListEvent(IStringList sender);
+//    public interface IStringList
+//    {
+//        // void Add(string s);
+//        //       int Count { get; }
+//        //        event StringListEvent Changed;
+//        //         string this[int index] { get; set; }
+//    }
 
-    abstract public class ComPlusSecurity
-    {
-        abstract public void LogIn();
-        abstract public void LogOut();
-    }
+//    abstract public class ComPlusSecurity
+//    {
+//        abstract public void LogIn();
+//        abstract public void LogOut();
+//    }
 
-    public class Bad
-    {
-        public event ResolveEventHandler BeforeResolve;
-        public event ResolveEventHandler AfterResolve;
+//    public class Bad
+//    {
+//        public event ResolveEventHandler BeforeResolve;
+//        public event ResolveEventHandler AfterResolve;
+//
+//        private double _seed;
+//        public double Seed
+//        {
+//            // no get since there's no use case for it
+//            set
+//            {
+//                _seed = value;
+//            }
+//
+//            get { return _seed; }
+//        }
+//
+//        public bool this[DateTime date, int i1, long l2, string s3, double d1, float f2]
+//        {
+//            get
+//            {
+//                return false;
+//            }
+//        }
+//    }
+//
+//    public class HelloCollection : System.Collections.Stack
+//    {
+//        public int this[int x, int y]
+//        {
+//            get
+//            {
+//                return 0;
+//            }
+//        }
+//    }
 
-        private double _seed;
-        public double Seed
-        {
-            // no get since there's no use case for it
-            set
-            {
-                _seed = value;
-            }
+//    public enum Answer
+//    {
+//        AnswerYes,
+//        AnswerNo,
+//        AnswerMaybe,
+//    }
 
-            get { return _seed; }
-        }
+//    [Flags]
+//    [Serializable]
+//    enum Access
+//    {
+//        Read = 0,
+//        Write = 1
+//    }
 
-        public bool this[DateTime date, int i1, long l2, string s3, double d1, float f2]
-        {
-            get
-            {
-                return false;
-            }
-        }
-    }
-
-    public class HelloCollection : System.Collections.Stack
-    {
-        public int this[int x, int y]
-        {
-            get
-            {
-                return 0;
-            }
-        }
-    }
-
-    public enum Answer
-    {
-        AnswerYes,
-        AnswerNo,
-        AnswerMaybe,
-    }
-
-    [Flags]
-    [Serializable]
-    enum Access
-    {
-        Read = 0,
-        Write = 1
-    }
-
-    public abstract class ClsAttribute : Attribute
-    {
-
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
-    }
+//    public abstract class ClsAttribute : Attribute
+//    {
+//
+//        public void Dispose()
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
 
     class Program
     {
---
+++ src/RGendarme/RGendarme/RGendarme/Rules/Design/TypesWithNativeFieldsShouldBeDisposable/TypesWithNativeFieldsShouldBeDisposableAnalyzer.cs
@@ -0,0 +1,87 @@
+﻿using System.Linq;
+using JetBrains.Application.Settings;
+using JetBrains.ReSharper.Daemon;
+using JetBrains.ReSharper.Daemon.Stages;
+using JetBrains.ReSharper.Daemon.Stages.Dispatcher;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.Psi.CSharp.Parsing;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Tree;
+using RGendarme.Lib;
+
+namespace RGendarme.Rules.Design.TypesWithNativeFieldsShouldBeDisposable
+{
+    [ElementProblemAnalyzer(new[] { typeof(IClassDeclaration) }, HighlightingTypes = new[] { typeof(TypesWithNativeFieldsShouldBeDisposableHighlighting) })]
+    public class TypesWithNativeFieldsShouldBeDisposableAnalyzer : ElementProblemAnalyzer<IClassDeclaration>
+    {
+        private readonly ISettingsStore _settings;
+        public TypesWithNativeFieldsShouldBeDisposableAnalyzer(ISettingsStore settings)
+        {
+            _settings = settings;
+        }
+
+        protected override void Run(IClassDeclaration element, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
+        {
+            // 1. if class implements IDisposable and Dispose exists and is not abstract - return
+            if (element.ExtendsList == null || !AnalyzerHelper.IsImplement(element.ExtendsList, "System.IDisposable"))
+                return;
+
+            // TODO refactor this - move it to helper class
+            bool hasDispose = false; // if true - return
+            foreach (IMethodDeclaration m in element.MethodDeclarationsEnumerable)
+            {
+                
+                if (m.NameIdentifier != null && !string.IsNullOrEmpty(m.NameIdentifier.Name) &&
+                    m.NameIdentifier.Name.Equals("Dispose") && 
+                    !m.ModifiersList.HasModifier(CSharpTokenType.ABSTRACT_KEYWORD))
+                {
+                    hasDispose = true;
+                    break;
+                }
+            }
+
+            if (hasDispose)
+                return;
+
+            // 2. if class has  IntPtr, UIntPtr, or HandleRef fields - throw warning
+            var unsafeTypes = new[] {"System.IntPtr", "System.UIntPtr", "System.Runtime.InteropServices.HandleRef"};
+            bool hasUnsafeFields = false; //= element.FieldDeclarationsEnumerable.Any(field => field.IsUnsafe);
+            foreach (IFieldDeclaration field in element.FieldDeclarationsEnumerable)
+            {
+                string clrName = field.DeclaredElement.Type.ToString();
+                if (unsafeTypes.Any(clrName.Equals))
+                {
+                    hasUnsafeFields = true;
+                    break;
+                }
+            }
+
+            if (hasUnsafeFields)
+            {
+                consumer.AddHighlighting(new TypesWithNativeFieldsShouldBeDisposableHighlighting(element), element.NameIdentifier.GetDocumentRange(), element.GetContainingFile());
+            }
+
+            //throw new System.NotImplementedException();
+        }
+    }
+
+    [StaticSeverityHighlighting(Severity.WARNING, CSharpLanguage.Name)]
+    public class TypesWithNativeFieldsShouldBeDisposableHighlighting : IHighlighting
+    {
+        public IClassDeclaration Declaration { get; private set; }
+
+        public TypesWithNativeFieldsShouldBeDisposableHighlighting(IClassDeclaration declaration)
+        {
+            Declaration = declaration;
+        }
+
+        public bool IsValid()
+        {
+            return Declaration != null && Declaration.IsValid();
+        }
+
+        public string ToolTip { get { return "Design: types with native fields should be disposable."; } }
+        public string ErrorStripeToolTip { get { return ToolTip; } }
+        public int NavigationOffsetPatch { get { return 0; } }
+    }
+}
